bd8e82f7a8cbd6845aaa1b5f2a87bad38b5cd6d5
k0baya <132335755+k0baya@users.noreply.github.com>
2023-06-24 21:47:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const { spawn } = require('child_process');
 
+const script = spawn('sh', ['run.sh']);
+
+script.stdout.on('data', (data) => {
+  console.log(` ${data}`);
+});
+
+script.stderr.on('data', (data) => {
+  console.error(` ${data}`);
+});
+
+script.on('close', (code) => {
+  console.log(`Shell Process exited with code ${code}`);
+});
