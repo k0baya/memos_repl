be8f7aac975bf174d4ffb865bd66994e2b501d8f
k0baya <132335755+k0baya@users.noreply.github.com>
2023-06-24 22:19:14 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,12 @@
+if [ -d "builder" ]; then
+    echo "初始化中..."
+    cd builder
+      go build -o memos ./main.go
+    cd ..
+      cp -f builder/memos .
+      rm -rf builder
+else
+    echo "直接运行..."
+fi
+
 chmod +x memos && ./memos
